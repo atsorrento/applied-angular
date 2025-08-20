e834f0c31eef7c0afc82b7c81445c0f04b7d0cad
Stephen Spears <atriver@comcast.net>
2025-08-20 20:29:58 +0000

    end of day

--- instructor/src/counter/counter.routes.ts
+++ instructor/src/counter/counter.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { Counter } from './counter';
-import { Ui } from './pages/ui';
 import { Prefs } from './pages/prefs';
+import { Ui } from './pages/ui';
+import { CounterStore } from './stores/counter';
 export const COUNTER_ROUTES: Routes = [
   {
     path: '',
     component: Counter,
+    providers: [CounterStore], // provide it here, create it on first inject, but then keep it in memory until the browser is closed.
     children: [
       {
         path: 'ui',
